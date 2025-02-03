75f170c5158b1695d764f04e09db1c956497bf10
luiscmogrovejo <luis.c.mogrovejo@gmail.com>
2025-02-03 13:21:34 -0500

    add github packages

--- release.config.cjs
+++ release.config.cjs
@@ -1,11 +1,26 @@
 // release.config.js
 module.exports = {
-  branches: ["main", { name: "next", prerelease: true }],
+  branches: ['main', { name: 'next', prerelease: true }],
   plugins: [
-    "@semantic-release/commit-analyzer",
-    "@semantic-release/release-notes-generator",
-    "@semantic-release/changelog",
-    "@semantic-release/npm",
-    "@semantic-release/github",
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/changelog',
+    // 1) Publish to npmjs.org
+    [
+      '@semantic-release/npm',
+      {
+        npmPublish: true,
+        registry: 'https://registry.npmjs.org',
+      },
+    ],
+    // 2) Publish to GitHub Packages
+    [
+      '@semantic-release/npm',
+      {
+        npmPublish: true,
+        registry: 'https://npm.pkg.github.com',
+      },
+    ],
+    '@semantic-release/github',
   ],
 };
