445dab8899e95138740dcc14a854caff8089f4a9
luiscmogrovejo <luis.c.mogrovejo@gmail.com>
2025-02-03 13:28:51 -0500

    feat: fix releases

--- release.config.cjs
+++ release.config.cjs
@@ -1,17 +1,11 @@
 // release.config.js
 module.exports = {
-  branches: ['main', { name: 'next', prerelease: true }],
+  branches: ["main", { name: "next", prerelease: true }],
   plugins: [
-    '@semantic-release/commit-analyzer',
-    '@semantic-release/release-notes-generator',
-    '@semantic-release/changelog',
-    [
-      '@semantic-release/npm',
-      {
-        npmPublish: true,
-        registry: 'https://registry.npmjs.org',
-      },
-    ],
-    '@semantic-release/github',
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/changelog",
+    "@semantic-release/npm",
+    "@semantic-release/github",
   ],
 };
