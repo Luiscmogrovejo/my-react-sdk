a7d0dc287a634df9a686aa63b7140e7879b415dd
luiscmogrovejo <luis.c.mogrovejo@gmail.com>
2025-02-03 13:26:01 -0500

    feat: new package creation

--- release.config.cjs
+++ release.config.cjs
@@ -5,20 +5,11 @@ module.exports = {
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
     '@semantic-release/changelog',
-    // 1) Publish to npmjs.org
     [
       '@semantic-release/npm',
       {
         npmPublish: true,
         registry: 'https://registry.npmjs.org',
-      },
-    ],
-    // 2) Publish to GitHub Packages
-    [
-      '@semantic-release/npm',
-      {
-        npmPublish: true,
-        registry: 'https://npm.pkg.github.com',
       },
     ],
     '@semantic-release/github',
